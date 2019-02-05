8c56de2aaed4a4dcfe64490de111de735970afba
gASK13 <gask13@gmail.com>
2019-02-05 13:39:21 +0100

    Item command.

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ bot.on('message', function (msg) {
     if (msg.content.toLowerCase().includes("good bot")) {
         msg.channel.send("Thanks! I try my best!");
     } else if (msg.mentions.users.exists('id', bot.user.id)) {
-       msg.channel.send("Hello there! You called me? If you wanna know how to interact with me properly, type in `]help` and I will tell you!");
+        msg.channel.send("Hello there! You called me? If you wanna know how to interact with me properly, type in `]help` and I will tell you!");
     } else if (msg.content.substring(0, 1) === ']') {
         // This is the magic - command parsing!
         let args = msg.content.substring(1).split(' ');
@@ -51,26 +51,6 @@ bot.on('message', function (msg) {
                 com.run(msg, args);
                 return;
             }
-        }
-
-        switch(cmd) {
-            case "item":
-                if (args.length < 3) {
-                    msg.channel.send("Usage: ]item {name} {type} [{weight} {cost}]");
-                    msg.channel.send("Supported types: food | resource | potion | weapon");
-                } else {
-                    switch(args[2]) {
-                        case "potion":
-                        case "resource":
-                        case "food":
-                            msg.channel.send('```xml\n<item id="' + args[1].replace("@", "") + '" type="' + (args.length > 2 ? args[2].replace("@", "") : "food") + '" cost="' + (args.length > 4 ? args[4].replace("@", "") : "0") + '" weight="' + (args.length > 3 ? args[3].replace("@", "") : "0") + '" icon="' + args[1].replace("@", "") + '.ico" />```');
-                            break;
-                        case "weapon":
-                            msg.channel.send('```xml\n<item id="' + args[1].replace("@", "") + '" type="equipment" slot="weapon" durability="500" cost="' + (args.length > 4 ? args[4].replace("@", "") : "0") + '" weight="' + (args.length > 3 ? args[3].replace("@", "") : "0") + '" attack="0" cut="0" stamina="0" knockback="0" icon="' + args[1].replace("@", "") + '.ico" animation="' + args[1].replace("@", "") + '" action="equip" movement_walk="walk_rswing" critical="false" />```');
-                            break;
-                    }
-                }
-              break;
         }
     }
 });
---
+++ commands/item.js
@@ -0,0 +1,30 @@
+const Command = require('../command.js');
+
+class ItemCommand extends Command {
+    constructor() {
+        super("Item", ['item'], 2);
+    }
+
+    help(msg) {
+        return "Usage: `]item {name} {type} [{weight} {cost}]`\nSupported types: `food | resource | potion | weapon`";
+    }
+
+    shortHelp(msg) {
+        return "helps with basic item syntax";
+    }
+
+    runInternal(msg, args) {
+        switch(args[2]) {
+            case "potion":
+            case "resource":
+            case "food":
+                msg.channel.send('```xml\n<item id="' + args[1].replace("@", "") + '" type="' + (args.length > 2 ? args[2].replace("@", "") : "food") + '" cost="' + (args.length > 4 ? args[4].replace("@", "") : "0") + '" weight="' + (args.length > 3 ? args[3].replace("@", "") : "0") + '" icon="' + args[1].replace("@", "") + '.ico" />```');
+                break;
+            case "weapon":
+                msg.channel.send('```xml\n<item id="' + args[1].replace("@", "") + '" type="equipment" slot="weapon" durability="500" cost="' + (args.length > 4 ? args[4].replace("@", "") : "0") + '" weight="' + (args.length > 3 ? args[3].replace("@", "") : "0") + '" attack="0" cut="0" stamina="0" knockback="0" icon="' + args[1].replace("@", "") + '.ico" animation="' + args[1].replace("@", "") + '" action="equip" movement_walk="walk_rswing" critical="false" />```');
+                break;
+        }
+    }
+}
+
+module.exports = ItemCommand;
