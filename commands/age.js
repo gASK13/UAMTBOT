c7cfb75bf479c683650d47ecd3ae437302b405fb
gASK13 <32205999+gASK13@users.noreply.github.com>
2019-02-12 22:39:07 +0100

    Tech's wish is granted (age for others, yay!)

--- commands/age.js
+++ commands/age.js
@@ -1,4 +1,5 @@
 const Command = require('../command.js');
+const UserService = require('../services/user.js');
 
 class AgeCommand extends Command {
     constructor() {
@@ -6,7 +7,7 @@ class AgeCommand extends Command {
     }
 
     help(msg) {
-        return "It's as simple as they come - just type ]age and I'll tell you how many days, hours or minutes you are pestering people on this server!";
+        return "It's as simple as they come - just type `]age` and I'll tell you how many days, hours or minutes you are pestering people on this server!\nYou can also check other peoples ages by using `]age {user}`...";
     }
 
     shortHelp(msg) {
@@ -14,12 +15,18 @@ class AgeCommand extends Command {
     }
 
     runInternal(msg, args) {
-        msg.guild.fetchMember(msg.author).then(function(value) {
+        let user = UserService.getUser(msg);
+        if (args.length >= 2) {
+            // find user
+            user = UserService.lookupUser(msg, args.slice(1).join(" "));
+            if (user == null) { return; }
+        }
+        msg.guild.fetchMember(user).then(function(value) {
             let ms = new Date().getTime() - value.joinedTimestamp;
             let min = Math.floor(ms / (1000 * 60));
             let hr = Math.floor(min / 60); min -= hr * 60;
             let ds = Math.floor(hr / 24); hr -= ds * 24;
-            msg.channel.send("You are a member of this server for " + ((ds > 0) ? (ds + " days, ") : "") + ((ds > 0 || hr > 0) ? (hr + " hours and ") : "") + min + " minutes.");
+            msg.channel.send((user.id == msg.author.id ? "You are" : UserService.getUsername(user) + " is") + " a member of this server for " + ((ds > 0) ? (ds + " days, ") : "") + ((ds > 0 || hr > 0) ? (hr + " hours and ") : "") + min + " minutes.");
         });
     }
 }
