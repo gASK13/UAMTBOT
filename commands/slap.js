529fa467dac46b78d3b1648168b219f96020fbc0
gASK13 <gask13@gmail.com>
2019-03-29 20:57:53 +0100

    Alchemist's wish is my command.

--- commands/slap.js
+++ commands/slap.js
@@ -75,6 +75,8 @@ class SlapCommand extends Command {
 
         if (user.id === '522160089554092041') {
             msg.channel.send(UserService.getUsernameFromMessage(msg) + " has tried to slap [UAMT]Bot. [UAMT]Bot evaded. [UAMT]Bot has slapped " + UserService.getUsernameFromMessage(msg) + " so hard that " + UserService.getUsernameFromMessage(msg) + " fainted!");
+        } else if (user.id === '500774841738199070') {
+            msg.channel.send(UserService.getUsernameFromMessage(msg) + " has tried to slap Alchemist. Alchemist teleported behind " + UserService.getUsernameFromMessage(msg) + ". Alchemist used a magic hand to slap " + UserService.getUsernameFromMessage(msg) + " into the next week.");
         } else if (user.id === msg.author.id) {
             msg.channel.send(UserService.getUsernameFromMessage(msg) + " really likes to slap themselves. Nudge nudge wink wink.");
         } else {
