8341ffb7faf00d839d201b028e61aa01402af3db
gASK13 <gask13@gmail.com>
2019-03-29 11:27:43 +0100

    Some silliness - refactor.

--- commands/slap.js
+++ commands/slap.js
@@ -21,38 +21,38 @@ class SlapCommand extends Command {
 
     runInternal(msg, args) {
         let uname = args.slice(1).join(" ").replace("@", "");
-        if (uname === 'me') {
-            msg.channel.send("Oh, " + UserService.getUsernameFromMessage(msg) + ", silly you! You cannot slap yourself. Or can you?");
-        } else {
-            let user = UserService.lookupUser(msg, uname);
-            if (user == null) {
-                switch (uname) {
-                    case "water":
-                        msg.channel.send("Splash!");
-                        break;
-                    case "stone":
-                        msg.channel.send("Ow! " + UserService.getUsernameFromMessage(msg) + " has broken their wrist!" );
-                        break;
-                    case "fire":
-                        msg.channel.send("Yikes! " + UserService.getUsernameFromMessage(msg) + " has burned their hand!" );
-                        break;
-                    case "earth":
-                        msg.channel.send("THUD!" );
-                        break;
-                    case "air":
-                        msg.channel.send("...... seriously?" );
-                        break;
-                }
+        switch (uname) {
+            case "water":
+                msg.channel.send("Splash!");
                 return;
-            }
+            case "stone":
+                msg.channel.send("Ow! " + UserService.getUsernameFromMessage(msg) + " has broken their wrist!" );
+                return;
+            case "fire":
+                msg.channel.send("Yikes! " + UserService.getUsernameFromMessage(msg) + " has burned their hand!" );
+                return;
+            case "earth":
+                msg.channel.send("THUD!" );
+                return;
+            case "air":
+                msg.channel.send("...... seriously?" );
+                return;
+            case "me":
+                msg.channel.send("Oh, " + UserService.getUsernameFromMessage(msg) + ", silly you! You cannot slap yourself. Or can you?");
+                return;
+        }
 
-            if (user.id === '522160089554092041') {
-                msg.channel.send(UserService.getUsernameFromMessage(msg) + " has tried to slap [UAMT]Bot. [UAMT]Bot evaded. [UAMT]Bot has slapped " + UserService.getUsernameFromMessage(msg) + " so hard that " + UserService.getUsernameFromMessage(msg) + " fainted!");
-            } else if (user.id === msg.author.id) {
-                msg.channel.send(UserService.getUsernameFromMessage(msg) + " really likes to slap themselves. Nudge nudge wink wink.");
-            } else {
-                msg.channel.send(UserService.getUsernameFromMessage(msg) + " has just slapped " + UserService.getUsername(user) + "!");
-            }
+        let user = UserService.lookupUser(msg, uname);
+        if (user == null) {
+            return;
+        }
+
+        if (user.id === '522160089554092041') {
+            msg.channel.send(UserService.getUsernameFromMessage(msg) + " has tried to slap [UAMT]Bot. [UAMT]Bot evaded. [UAMT]Bot has slapped " + UserService.getUsernameFromMessage(msg) + " so hard that " + UserService.getUsernameFromMessage(msg) + " fainted!");
+        } else if (user.id === msg.author.id) {
+            msg.channel.send(UserService.getUsernameFromMessage(msg) + " really likes to slap themselves. Nudge nudge wink wink.");
+        } else {
+            msg.channel.send(UserService.getUsernameFromMessage(msg) + " has just slapped " + UserService.getUsername(user) + "!");
         }
     }
 }
