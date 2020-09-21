14bca350376d45e14862d4ba176d7995fcd09a1b
gASK13 <gask13@gmail.com>
2020-09-21 09:18:03 +0200

    Age for alchemist independently of nick.

--- commands/age.js
+++ commands/age.js
@@ -22,7 +22,7 @@ class AgeCommand extends Command {
             if (user == null) {
                 return;
             }
-            if (user.id == '483028152130469891') {
+            if (user.id='500774841738199070') {
                 msg.channel.send("Alchemist is timeless and has always been a member of this server! All hail the Alchemist!");
                 return;
             }
