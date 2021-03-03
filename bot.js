288a00d1e91f57b495d4cc5fcc63cef9be524497
gASK13 <32205999+gASK13@users.noreply.github.com>
2021-03-03 19:07:45 +0100

    Restart

--- bot.js
+++ bot.js
@@ -6,8 +6,6 @@ const ModService = require('./services/mods.js');
 const SteamService = require('./services/steam.js');
 
 
-
-
 // BOT SETUP
 let bot = new Discord.Client({});
 bot.on("error", (e) => console.error(e));
